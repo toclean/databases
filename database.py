3af85cbb6c5204c6cc02d076d68ba3067f06c8fd
toclean <connormoorebusiness@gmail.com>
2018-04-23 17:49:56 -0500

    More progress on person

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 import mysql.connector as mariadb
 import uuid
+import random
 
 # load firstnames
 with open('first-names') as f:
-    data = [x.strip() for x in f.readlines()]
+    firstnames = [x.strip() for x in f.readlines()]
+
+# load lastnames
+with open('last-names') as f:
+    lastnames = [x.strip() for x in f.readlines()]
 
 # print (data)
 
@@ -27,10 +32,10 @@ db = mariadb.connect(host="127.0.0.1", user="root", password=passwd, database="d
 cursor = db.cursor()
 
 for i in range(0, 200):
-    print (personUids[i], data[i])
-    sql = "insert into person (PersonUid, Active, FirstName) VALUES ('%s', 1, '%s')" % (personUids[i], data[i])
+    # print (personUids[i], data[i])
+    sql = "insert into person (PersonUid, Active, FirstName, LastName, MiddleName) VALUES ('%s', 1, '%s', '%s', '%s')" % (personUids[i], firstnames[random.randint(0, 4700)], lastnames[random.randint(0, 4700)], firstnames[random.randint(0, 4700)])
     print (sql)
-    data = cursor.execute(sql)
+    output = cursor.execute(sql)
     # print (data)
     db.commit()
 
