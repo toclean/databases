629f454f83b4fc311cf3bbb2c9019178c97a1d74
toclean <connormoorebusiness@gmail.com>
2018-04-23 19:03:46 -0500

    More changes to person script and updated sql

---
+++ UpdatedDump.sql
@@ -0,0 +1,246 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: dbproject
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctor` (
+  `DoctorUid` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `NpiNumber` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DEANumber` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`DoctorUid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctor`
+--
+
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hospital`
+--
+
+DROP TABLE IF EXISTS `hospital`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hospital` (
+  `HospitalUid` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `NumberOfRooms` int(11) DEFAULT NULL,
+  PRIMARY KEY (`HospitalUid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hospital`
+--
+
+LOCK TABLES `hospital` WRITE;
+/*!40000 ALTER TABLE `hospital` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hospital` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hospitalemployee`
+--
+
+DROP TABLE IF EXISTS `hospitalemployee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hospitalemployee` (
+  `HEmployeeUid` varchar(32) NOT NULL,
+  `Qualification` varchar(45) NOT NULL,
+  `Salary` double DEFAULT NULL,
+  PRIMARY KEY (`HEmployeeUid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hospitalemployee`
+--
+
+LOCK TABLES `hospitalemployee` WRITE;
+/*!40000 ALTER TABLE `hospitalemployee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hospitalemployee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `medication`
+--
+
+DROP TABLE IF EXISTS `medication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `medication` (
+  `MedicationUid` varchar(32) NOT NULL,
+  `Code` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `Dosage` varchar(45) DEFAULT NULL,
+  `Description` varchar(45) DEFAULT NULL,
+  `UnitsPerDay` varchar(45) DEFAULT NULL,
+  `Start` varchar(45) DEFAULT NULL,
+  `End` varchar(45) DEFAULT NULL,
+  `Method` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`MedicationUid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `medication`
+--
+
+LOCK TABLES `medication` WRITE;
+/*!40000 ALTER TABLE `medication` DISABLE KEYS */;
+/*!40000 ALTER TABLE `medication` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nurse`
+--
+
+DROP TABLE IF EXISTS `nurse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nurse` (
+  `NurseUid` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DoctorUid` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `PatientCount` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`NurseUid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nurse`
+--
+
+LOCK TABLES `nurse` WRITE;
+/*!40000 ALTER TABLE `nurse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nurse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `patient` (
+  `PatientUid` varchar(32) NOT NULL,
+  `MedicalRecordNumber` varchar(45) DEFAULT NULL,
+  `ArrivalDate` date NOT NULL,
+  `ReleaseDate` date DEFAULT NULL,
+  `Condition` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`PatientUid`),
+  CONSTRAINT `FK_PatientUid` FOREIGN KEY (`PatientUid`) REFERENCES `person` (`personuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `person`
+--
+
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person` (
+  `Active` tinyint(4) NOT NULL,
+  `PersonUid` varchar(32) NOT NULL,
+  `FirstName` varchar(45) DEFAULT NULL,
+  `LastName` varchar(45) DEFAULT NULL,
+  `MiddleName` varchar(45) DEFAULT NULL,
+  `Ssn` int(11) DEFAULT NULL,
+  `BirthDate` date DEFAULT NULL,
+  `DeceasedBool` tinyint(4) DEFAULT NULL,
+  `Gender` varchar(45) DEFAULT NULL,
+  `Address` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`PersonUid`),
+  UNIQUE KEY `Ssn_UNIQUE` (`Ssn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+INSERT INTO `person` VALUES (1,'0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room` (
+  `RoomUid` varchar(35) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `RoomNumber` int(11) NOT NULL,
+  `Occupied` tinyint(4) NOT NULL,
+  `RoomType` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `NumberOfBeds` int(11) NOT NULL,
+  PRIMARY KEY (`RoomUid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'dbproject'
+--
+
+--
+-- Dumping routines for database 'dbproject'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-23 16:38:22
--- database.py
+++ database.py
@@ -17,8 +17,23 @@ def genUids(list):
         list.append(str(uuid.uuid4()))
 
 personUids = []
+ssns = []
 
-# Create list of personUids
+for i in range(0, 200):
+    rnums = ""
+    firstdashflag = 0;
+    for j in range(0, 9):
+        if (firstdashflag == 0 and j == 3):
+            firstdashflag = 1;
+            rnums += "-"
+        if (firstdashflag == 1 and j == 5):
+            rnums += "-"
+        rnums += str(random.randint(0, 9))
+    ssns.append(rnums)
+
+print (ssns)
+
+# Create list of personUid
 genUids(personUids)
 
 with open('creds') as f:
@@ -28,13 +43,12 @@ passwd = creds[0]
 
 db = mariadb.connect(host="127.0.0.1", user="root", password=passwd, database="dbproject");
 
-
 cursor = db.cursor()
 
 for i in range(0, 200):
     # print (personUids[i], data[i])
     sql = "insert into person (PersonUid, Active, FirstName, LastName, MiddleName) VALUES ('%s', 1, '%s', '%s', '%s')" % (personUids[i], firstnames[random.randint(0, 4700)], lastnames[random.randint(0, 4700)], firstnames[random.randint(0, 4700)])
-    print (sql)
+    # print (sql)
     output = cursor.execute(sql)
     # print (data)
     db.commit()
