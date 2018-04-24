9d74ef98800f6681a21a5cbc1e8eeb99f1f1f159
toclean <connormoorebusiness@gmail.com>
2018-04-23 19:52:23 -0500

    Progress on person

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import mysql.connector as mariadb
 import uuid
 import random
+import time
 
 # load firstnames
 with open('first-names') as f:
@@ -31,10 +32,48 @@ for i in range(0, 200):
         rnums += str(random.randint(0, 9))
     ssns.append(rnums)
 
-print (ssns)
+# print (ssns)
 
 # Create list of personUid
 genUids(personUids)
+
+# generate random birthdates
+def dateProp(start, end, format, prop):
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+def randomDate(start, end, prop):
+    return dateProp(start, end, '%Y-%m-%d', prop)
+
+dates = []
+
+for i in range(0, 200):
+    dates.append(randomDate("1960-01-01", "2018-01-01", random.random()))
+
+print (dates[i])
+
+deaths = []
+
+for i in range(0, 200):
+    chance = random.randint(0, 50)
+    if (chance < 48):
+        death = 0;
+    else:
+        death = 1;
+    deaths.append(death);
+
+genders = []
+
+for i in range(0, 200):
+    chance = random.randint(0, 10)
+    if (chance <= 5):
+        genders.append("Male");
+    else:
+        genders.append("Female");
 
 with open('creds') as f:
     creds = [x.strip() for x in f.readlines()]
@@ -47,7 +86,7 @@ cursor = db.cursor()
 
 for i in range(0, 200):
     # print (personUids[i], data[i])
-    sql = "insert into person (PersonUid, Active, FirstName, LastName, MiddleName) VALUES ('%s', 1, '%s', '%s', '%s')" % (personUids[i], firstnames[random.randint(0, 4700)], lastnames[random.randint(0, 4700)], firstnames[random.randint(0, 4700)])
+    sql = "insert into person (PersonUid, Active, FirstName, LastName, MiddleName, Ssn, BirthDate, DeceasedBool, Gender) VALUES ('%s', 1, '%s', '%s', '%s', '%s', '%s', %i, '%s')" % (personUids[i], firstnames[random.randint(0, 4700)], lastnames[random.randint(0, 4700)], firstnames[random.randint(0, 4700)], ssns[i], dates[i], deaths[i], genders[i])
     # print (sql)
     output = cursor.execute(sql)
     # print (data)
